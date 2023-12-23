a01df86174df6fcab7132ed3e745b1411e875fce
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-12-24 03:23:38 +0800

    Added path on tsconfig and alias on vite.config

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import path from "path";
 
 export default defineConfig({
   plugins: [solid()],
 })
+  resolve: {
+    alias: {
+      "@console/functions": path.resolve(__dirname, "../../functions/src"),
+      "@console/core": path.resolve(__dirname, "../../core/src"),
+      $: path.resolve(__dirname, "./src"),
+    },
+  },
+});
