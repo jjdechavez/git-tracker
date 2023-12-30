4af8bac4d80d9dfd5edbd86e0662bb4a8c6e5f7f
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-12-30 10:36:59 +0800

    Implement authentication on web

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,12 +1,40 @@
-import { Router, Route } from "@solidjs/router";
+import { Router, Route, useLocation, Navigate } from "@solidjs/router";
+import { Match, Switch } from "solid-js";
 import SigninRoute from "./routes/signin";
+import { useStorage } from "./providers/account";
+import HomeRoute from "./routes/home";
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={() => <div>Home Page</div>} />
+      <Route path="/" component={HomeRoute} />
       <Route path="/about" component={() => <div>About Page</div>} />
       <Route path="/signin" component={SigninRoute} />
+      <Route
+        path="/oauth/github"
+        component={() => {
+          const storage = useStorage();
+          const location = useLocation();
+
+          const search = location.search;
+          const params = new URLSearchParams(search);
+          const signinToken = params.get("token");
+          if (signinToken) {
+            storage.set("token", signinToken);
+          }
+
+          return (
+            <Switch>
+              <Match when={signinToken}>
+                <Navigate href={"/"} />
+              </Match>
+              <Match when={!signinToken}>
+                <Navigate href={"/signin"} />
+              </Match>
+            </Switch>
+          );
+        }}
+      />
     </Router>
   );
 }
