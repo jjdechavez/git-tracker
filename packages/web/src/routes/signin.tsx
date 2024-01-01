bda1c432a0a706a6d3d9016d7b8705aea63e20f2
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2024-01-01 15:09:02 +0800

    Updated signin router

--- packages/web/src/routes/signin.tsx
+++ packages/web/src/routes/signin.tsx
@@ -1,23 +1,28 @@
 export default function SigninRoute() {
   return (
-    <main class="container max-w-md min-h-min flex flex-col items-center justify-center mx-auto p-6">
-      <div class="rounded-lg border bg-card text-card-foreground shadow-sm">
-        <header class="flex flex-col space-y-1.5 p-6">
-          <h1 class="text-2xl font-semibold leading-none tracking-tight">
-            Login
+    <section class="container mx-auto max-w-3xl px-6">
+      <div class="px-5 py-56 mx-auto">
+        <div class="flex flex-col text-center w-full mb-10">
+          <h1 class="text-lg font-medium title-font text-white mb-2">
+            Welcome to the Git Tracker
           </h1>
-          <p class="text-sm text-muted-foreground">test</p>
-        </header>
+          <p class="text-base leading-relaxed">
+            Sign in with your github to get started
+          </p>
+        </div>
 
-        <div class="p-6 pt-0">
-          <a
-            href={`${import.meta.env.VITE_APP_API_URL}/auth/github/authorize`}
-            rel="noreferrer"
-          >
-            Sign in with Github
-          </a>
+        <div class="lg:w-1/2 md:w-2/3 mx-auto">
+          <div class="grid gap-y-4">
+            <a
+              href={`${import.meta.env.VITE_APP_API_URL}/auth/github/authorize`}
+              rel="noreferrer"
+              class="w-full text-center text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded"
+            >
+              Sign in with Github
+            </a>
+          </div>
         </div>
       </div>
-    </main>
+    </section>
   );
 }
