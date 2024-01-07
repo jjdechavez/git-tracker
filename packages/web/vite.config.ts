7e6d61a2540e5835188ca3807167635072507baf
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2024-01-07 14:18:38 +0800

    Added devtool plugin on vite

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import path from "path";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [devtools({ autoname: true }), solid()],
   resolve: {
     alias: {
       "@git-tracker/functions": path.resolve(__dirname, "../../functions/src"),
